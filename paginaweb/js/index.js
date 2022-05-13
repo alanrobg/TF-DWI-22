2679583d7cd4184b57d75a0aa8feb090a50ea0ef
alanrobg <alanrobg@gmail.com>
2022-05-13 18:46:16 -0500

    creacion de carta

---
+++ paginaweb/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+    
+    M.AutoInit();
+    $('.slider').bxSlider({
+        auto: true,
+        stopAutoOnClick: true,
+        pager: true,
+        controls: false,
+        });
+    
+})
